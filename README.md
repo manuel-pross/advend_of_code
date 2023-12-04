26cb066a054b9a3282472e37638bee88af40a2f4
Manuel Proß <manuel.pross@getflip.com>
2023-12-04 09:04:58 +0100

    feat: finish second assignment first task

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 <!--SOLUTIONS-->
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%86/yellow)](src/day01)
-![Day](https://badgen.net/badge/02/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%86/yellow)](src/day02)
 ![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
@@ -69,16 +69,16 @@ npm start 1
 
 ```
 Day 01
-Time part 1: 0.981ms
+Time part 1: 0.974ms
 Time part 2: -
-Both parts: 0.981ms
+Both parts: 0.974ms
 ```
 
 ```
 Day 02
-Time part 1: -
+Time part 1: 0.567ms
 Time part 2: -
-Both parts: -
+Both parts: 0.567ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 1/50
-Total time: 0.981ms
+Total stars: 2/50
+Total time: 1.541ms
 ```
 
 <!--/RESULTS-->
--- src/day02/index.ts
+++ src/day02/index.ts
@@ -24,21 +24,77 @@ const testInput =
   "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
 
 const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  const lines = input.split("\n");
+
+  const games = generateGames(lines);
+
+  let acc = 0;
+
+  games.forEach((game) => {
+    if (game.isValid) {
+      acc += game.id;
+    }
+  });
+
+  return acc;
+};
+
+function generateGames(input: string[]): Game[] {
+  const games: Game[] = new Array(input.length);
+
+  input.forEach((line, index) => {
+    games[index] = {
+      id: 0,
+      gameparts: [],
+      isValid: true,
+    };
+    games[index].id = index + 1;
+
+    const gameparts = line.slice(line.indexOf(":") + 2);
+    const gamepartsSeparatedBySemi = gameparts.split(";");
+
+    gamepartsSeparatedBySemi.forEach((gamepart, indexGamepart) => {
+      games[index].gameparts.push({
+        blue: 0,
+        red: 0,
+        green: 0,
+      });
+      const separatedByComma = gamepart.split(",");
+
+      for (const color of separatedByComma) {
+        if (color.includes("blue")) {
+          games[index].gameparts[indexGamepart].blue = +color.match(/\d+/)[0];
+        } else if (color.includes("red")) {
+          games[index].gameparts[indexGamepart].red = +color.match(/\d+/)[0];
+        } else if (color.includes("green")) {
+          games[index].gameparts[indexGamepart].green = +color.match(/\d+/)[0];
+        }
+
+        if (
+          games[index].gameparts[indexGamepart].blue > threshold.blue ||
+          games[index].gameparts[indexGamepart].red > threshold.red ||
+          games[index].gameparts[indexGamepart].green > threshold.green
+        ) {
+          games[index].isValid = false;
+          break;
+        }
+      }
+    });
+  });
+
+  //console.log(JSON.stringify(games));
+
+  return games;
+}
+
+const part2 = (rawInput: string) => {
   //const input = parseInput(rawInput);
   const input = testInput;
   const lines = input.split("\n");
 
   const games = generateGames(lines);
-
-  return;
-};
-
-function generateGames(input: string[]): void {
-  return;
-}
-
-const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+  console.log(games);
 
   return;
 };
